4631df1527254fedad72b074144671277958f0ad
RobDanHill <danielhill10@bellsouth.net>
2022-05-14 17:53:17 -0400

    fix(node_nuke): let clap set PATH default to current directory

--- node_nuke/src/args.rs
+++ node_nuke/src/args.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use std::{env, path::PathBuf};
+use std::path::PathBuf;
 
 #[derive(Parser)]
 #[clap(name = "Node Nuke")]
@@ -7,8 +7,8 @@ use std::{env, path::PathBuf};
 pub struct Args {
     /// Path to a directory containing a `node_modules` folder.
     /// Defaults to the current directory if not provided.
-    #[clap(parse(from_os_str))]
-    path: Option<PathBuf>,
+    #[clap(parse(from_os_str), default_value = ".")]
+    pub path: PathBuf,
     /// Remove all associated lock files
     /// (e.g. `package-lock.json`, `yarn.lock`, `pnpm-lock.yaml`).
     #[clap(long, short = 'D')]
@@ -20,14 +20,8 @@ const NODE_MODULES: &str = "node_modules";
 const LOCK_FILE_NAMES: &[&str] = &["package-lock.json", "yarn.lock", "pnpm-lock.yaml"];
 
 impl Args {
-    fn get_path(&self) -> PathBuf {
-        self.path
-            .clone()
-            .unwrap_or_else(|| env::current_dir().expect("Couldn't get CWD for some reason"))
-    }
-
     pub fn get_node_modules_path(&self) -> Option<PathBuf> {
-        let node_modules_path = self.get_path().join(NODE_MODULES);
+        let node_modules_path = self.path.join(NODE_MODULES);
         (node_modules_path.exists() && node_modules_path.is_dir()).then(|| node_modules_path)
     }
 
@@ -35,7 +29,7 @@ impl Args {
         LOCK_FILE_NAMES
             .iter()
             .filter_map(|file_nm| {
-                let lock_file = self.get_path().join(file_nm);
+                let lock_file = self.path.join(file_nm);
                 lock_file.exists().then(|| lock_file)
             })
             .collect::<Vec<PathBuf>>()
@@ -47,39 +41,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn when_get_path() {
-        // Arrange
-        let expected = PathBuf::from("/some/test/path");
-        let data = Args {
-            path: Some(PathBuf::from("/some/test/path")),
-            remove_lock: false,
-        };
-        // Act
-        let actual = data.get_path();
-        // Assert
-        assert_eq!(actual, expected);
-    }
-
-    #[test]
-    fn when_get_path_default() {
-        // Arrange
-        let expected = env::current_dir().expect("Couldn't get CWD for some reason");
-        let data = Args {
-            path: None,
-            remove_lock: true,
-        };
-        // Act
-        let actual = data.get_path();
-        // Assert
-        assert_eq!(actual, expected);
-    }
-
-    #[test]
     fn when_get_node_modules_path() {
         // Arrange
         let expected = None;
         let data = Args {
-            path: Some(PathBuf::from("/some/test/path")),
+            path: PathBuf::from("/some/test/path"),
             remove_lock: true,
         };
         // Act
@@ -93,7 +59,7 @@ mod tests {
         // Arrange
         let expected: Vec<PathBuf> = Vec::new();
         let data = Args {
-            path: Some(PathBuf::from("/some/test/path")),
+            path: PathBuf::from("/some/test/path"),
             remove_lock: true,
         };
         // Act
